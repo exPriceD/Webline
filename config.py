27da00443a4bc5dbac93d94995a6e63598f43cd1
exPriceD <lolerpro4@gmail.com>
2023-10-03 21:23:13 +0300

    +mail

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-from flask import Flask, session
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 from datetime import timedelta
 import os
 
@@ -19,16 +20,18 @@ application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 global COOKIE_TIME_OUT
 COOKIE_TIME_OUT = 60*60*24*7 #7 days
 
-'''
+
 application.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
 application.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
 application.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL')
 application.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 application.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 application.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-'''
+
 
 db = SQLAlchemy(application)
 
 login_manager = LoginManager(application)
 login_manager.login_view = 'login'
+
+mail = Mail(application)
