b751dcc0ec91dedcb47e42d7a3fc2e6846c8835b
exPriceD <lolerpro4@gmail.com>
2023-10-04 10:29:25 +0300

    +bug fix

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ from flask_login import UserMixin
 class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.Integer())
-    title = db.Column(db.String(500), unique=True)
-    description = db.Column(db.String(15000), nullable=True)
+    title = db.Column(db.String(500))
+    description = db.Column(db.String(15000))
     price = db.Column(db.String(50))
     status = db.Column(db.String(300))
     payment_status = db.Column(db.String(300))
